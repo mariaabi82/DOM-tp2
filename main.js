c301911f6777bcdb66463e4d10c0dc6bc95a3108
pierifrancia  <pieri.francia@gmail.com>
2020-03-02 22:00:31 -0300

    corregimos modal en JS

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
-fetch('https://tp-js-2-api-wjfqxquokl.now.sh/users')
+const mostrarUsuarios = () => {
+  console.log('mostrar')
+
+  fetch('https://tp-js-2-api-wjfqxquokl.now.sh/users')
     .then(data => data.json())
     .then(result => {
-        console.log(result)
-        const rowEmployee = document.getElementById("employees")
+      console.log(result)
+      const rowEmployee = document.getElementById("employees")
 
-        let acc = "";
+      let acc = "";
 
-        result.forEach(employee => {
-            let name = employee.fullname
-            let email = employee.email
-            let address = employee.address
-            let phone = employee.phone
-            let actions = employee.actions
+      result.forEach(employee => {
+        let name = employee.fullname
+        let email = employee.email
+        let address = employee.address
+        let phone = employee.phone
+        let actions = employee.actions
 
-            acc += `<tr>
+        acc += `<tr>
        <td>${name}</td>
        <td>${email}</td>
        <td>${address}</td>
@@ -23,122 +26,128 @@ fetch('https://tp-js-2-api-wjfqxquokl.now.sh/users')
        <i class="fa fa-trash trash-icon" id="${employee.id}"></i>
        </td>   
        </tr>`
-        });
+      });
 
-        rowEmployee.innerHTML = ` <thead><tr>
+      rowEmployee.innerHTML = ` <thead><tr>
         <th>Name</th>
         <th>Email</th>
         <th>Address</th>
         <th>Phone</th>
         <th>Actions</th>
         </tr></thead>` + acc;
-       
-    
-
-    const pencil = document.getElementsByClassName("pencil-icon")
-    const trash = document.getElementsByClassName("trash-icon")
-    const submitButton = document.getElementById("submit-button")
-    const modal = document.getElementById("modal")
-
-    submitButton.onclick = () => {
-    modal.classList.remove('nomostrar')
-    }
-
-    const cancel = document.getElementById("cancel")
-
-    cancel.onclick = () => {
-      modal.classList.add('nomostrar')
-    }
-
-    const add = document.getElementById("add")
-
-    add.onclick = () => {
-      const name = document.getElementById("new-name").value
-      const email = document.getElementById("new-email").value
-      const adress = document.getElementById("new-adress").value
-      const phone = document.getElementById("new-phone").value
-      const newEmployee = {
-        fullname: `${name}`,
-        email: `${email}`,
-        address: `${adress}`,
-        phone: `${phone}`,
-        
-      };
-      
-      fetch(`https://tp-js-2-api-wjfqxquokl.now.sh/users`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(newEmployee),
-      })
-        .then(data => data.json())
-        .then(result => console.log(result));
-    }
-
-    /*pencil.onclick = () => {
 
 
-      fetch(`https://tp-js-2-api-wjfqxquokl.now.sh/users/edit/${2}`, {
-            method: 'PUT',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(modifiedEmployee),
-        })
-            .then(data => data.json())
-            .then(result => console.log(result));
 
-    }*/
+      const pencil = document.getElementsByClassName("pencil-icon")
+      const trash = document.getElementsByClassName("trash-icon")
+      const submitButton = document.getElementById("submit-button")
+      const modal = document.getElementById("modal")
 
-    for (let i = 0; i < trash.length; i++) {
-      trash[i].onclick = () => {
-        const remove = trash[i].id
-        fetch(`https://tp-js-2-api-wjfqxquokl.now.sh/users/${remove}`, {
-        method: 'DELETE',
-        headers: { 'Content-Type': 'application/json' },
-    })
-        .then(data => data.json())
-        .then(result => console.log(result));
-    }
+      submitButton.onclick = () => {
+
+        const mostrarModal = () => {
+          modal.innerHTML = `<div class="modal-form-title">
+        <h2>Add Employee</h2>
+    </div>
+    <form action="" method="get" class="modal-form">
+        <label>Name</label>
+        <input type="text" id="new-name">
+        <label>Email</label>
+        <input type="text" id="new-email">
+        <label>Adress</label>
+        <textarea name="Adress" rows="3" cols="30" id="new-adress">
+        </textarea> 
+        <label>Phone</label>
+        <input type="text" id="new-phone">
+        </form>
+        <div class="div-button">
+        <button id="cancel">Cancel</button>
+        <button id="add">Add</button></div>
+    </form>`
+          modal.classList.remove('nomostrar')
+
+          const cancel = document.getElementById("cancel")
+
+          cancel.onclick = () => {
+            modal.classList.add('nomostrar')
+          }
+
+          const add = document.getElementById("add")
+
+          add.onclick = () => {
+            const name = document.getElementById("new-name").value
+            const email = document.getElementById("new-email").value
+            const address = document.getElementById("new-adress").value
+            const phone = document.getElementById("new-phone").value
+            const newEmployee = {
+              fullname: `${name}`,
+              email: `${email}`,
+              address: `${address}`,
+              phone: `${phone}`,
+            };
+
+            modal.classList.add('nomostrar')
+            fetch(`https://tp-js-2-api-wjfqxquokl.now.sh/users`, {
+              method: 'POST',
+              headers: { 'Content-Type': 'application/json' },
+              body: JSON.stringify(newEmployee),
+            })
+              .then(data => data.json())
+              .then(result => {
+                fetch('https://tp-js-2-api-wjfqxquokl.now.sh/users')
+                  .then(info => info.json())
+                  .then(resultado => {
+                    mostrarUsuarios()
+
+                  })
+              })
+
+          }
+
+        }
+
+        mostrarModal()
+
       }
-      
-    
-      
 
-    /*const newEmployee = {
-        fullname: 'Estefania Avalos',
-        email: 'emlavalos92@gmail.com',
-        address: 'Cochabamba 546',
-        phone: '155386503',
-        
-      };
 
- 
-      fetch(`https://tp-js-2-api-wjfqxquokl.now.sh/users/remove/${3}`, {
-        method: 'DELETE',
-        headers: { 'Content-Type': 'application/json' },
-    })
-        .then(data => data.json())
-        .then(result => console.log(result));
-   
-      fetch(`https://tp-js-2-api-wjfqxquokl.now.sh/users`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(newEmployee),
-      })
-        .then(data => data.json())
-        .then(result => console.log(result));
+      for (let i = 0; i < pencil.length; i++) {
+        pencil[i].onclick = () => {
+          console.log(pencil[i])
+          const edit = pencil[i].id
+          console.log('cualquier cosa')
+          mostrarModal()
 
-        const modifiedEmployee = {
-            fullname: 'Tefi Avalos',
-            email: 'emlavalos92@gmail.com',
-            address: 'Cochabamba 546',
-            phone: '155386503',
-        };
-        fetch(`https://tp-js-2-api-wjfqxquokl.now.sh/users/edit/${2}`, {
-            method: 'PUT',
+          //   fetch(`https://tp-js-2-api-wjfqxquokl.now.sh/users/${edit}`, {
+          //     method: 'PUT',
+          //     headers: { 'Content-Type': 'application/json' },
+          //   })
+          //     .then(data => data.json())
+          //     .then(result => console.log(result));
+          // }
+        }
+      }
+
+
+      for (let i = 0; i < trash.length; i++) {
+        trash[i].onclick = () => {
+          const remove = trash[i].id
+          fetch(`https://tp-js-2-api-wjfqxquokl.now.sh/users/${remove}`, {
+            method: 'DELETE',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(modifiedEmployee),
-        })
-            .then(data => data.json())
-            .then(result => console.log(result));
+          })
+            .then(dataDelete => dataDelete.json())
+            .then(resultDelete => {
+              fetch('https://tp-js-2-api-wjfqxquokl.now.sh/users')
+                .then(infoDelete => infoDelete.json())
+                .then(resultadoDelete => {
+                  mostrarUsuarios()
 
-    */
-  })
+                })
+            })
+        }
+      }
+    })
+}
+
+mostrarUsuarios()
